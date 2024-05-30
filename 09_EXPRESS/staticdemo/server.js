fe30d09ba942d290f4e74add779445f74a680659
bharathguntreddi3 <guntreddibharath@gmail.com>
2024-05-30 22:19:23 +0530

    sessions and body parsing - post

--- 09_EXPRESS/staticdemo/server.js
+++ 09_EXPRESS/staticdemo/server.js
@@ -1,1 +1,15 @@
-pub
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = 8888 
+ 
+app.use(express.static('public')) // include folder containing the static files
+
+app.get('/', (req, res) => {
+   res.end("<h1>Static Files Demo <h1>")
+})
+ 
+
+app.listen(port, () => {
+  console.log(`Express Application listening on port ${port}`)
+})  
