3e6f1eecf643d5a68f1a66499b962e377e7098fd
bharathguntreddi3 <guntreddibharath@gmail.com>
2024-05-31 08:33:55 +0530

    db retrievel & actions

---
+++ 09_EXPRESS/deptapp/database.js
@@ -0,0 +1,9 @@
+
+let connectionDetails = {
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'apr30',
+}
+ 
+module.exports.connectionDetails = connectionDetails 
--- 09_EXPRESS/sessions/count.js
+++ 09_EXPRESS/sessions/count.js
@@ -8,12 +8,12 @@ var cookieSession = require('cookie-session')
 
 app.use(cookieSession({
   name: 'session-data',
-  keys: ['k1'],
+  keys: ['k1'], //encrypt keys
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
 
 app.get('/countViews', (req, res) => {
-  req.session.count = (req.session.count || 0) + 1
+  req.session.count = (req.session.count || 0) + 1  // shortcircuit-OR evaluation
   req.session.lastaccess = new Date().toString()
   res.end(`<h1>You viewed this page for ${req.session.count} times. </h1><h2> Last accessed at ${req.session.lastaccess}</h2>`)
 })
