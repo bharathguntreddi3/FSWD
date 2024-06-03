8c1aa2c98985452a0f8dafb6dab2b4358651184a
bharathguntreddi3 <guntreddibharath@gmail.com>
2024-06-03 19:02:25 +0530

    ignre dependencies

---
+++ upload.bat
@@ -0,0 +1,14 @@
+IF "%~1"=="" (
+    echo Commit message is required.
+    echo Usage: upload.bat "Your commit message"
+    exit /b 1
+)
+
+git add .
+
+git commit -m "%~1"
+
+git pull origin master
+
+git push origin master
+
