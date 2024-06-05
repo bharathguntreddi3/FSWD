62421afd11826e7ee18245a117b818272550f6f4
bharathguntreddi3 <guntreddibharath@gmail.com>
2024-06-05 13:42:45 +0530

    ignored

--- upload.bat
+++ upload.bat
@@ -1,16 +1,8 @@
-IF "%~1"=="" (
-    echo Commit message is required.
-    echo Usage: upload.bat "Your commit message"
-    exit /b 1
-)
-
 git status
 
 git add .
 
-git commit -m "%~1"
-
-git pull origin master
+git commit -m %1
 
 git push origin master
 
